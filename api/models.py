94dbb4933dd42578c4c3efa5176b0bac40ba7e52
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-01-25 07:59:58 -0600

    commit

--- api/models.py
+++ api/models.py
@@ -5,11 +5,23 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
+    
+    def __str__ (self):
+        return self.title
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    
+    # def __str__ (self):
+    #     return self.stars
+    
+    def __str__ (self):
+        return f'Title: {self.movie} / Stars: {self.stars}'
+    
+    
     class Meta:
         unique_together = (('user', 'movie'),)
         index_together = (('user', 'movie'),)
+
