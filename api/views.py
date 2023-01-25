ec6abddfb4f6f58365497550153fa69550f362b2
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-01-24 20:07:44 -0600

    commit

--- api/views.py
+++ api/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
+    
+    @action(detail=True, methods=['Post'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            #user = request.user
+            user = User.objects.get(id=1)
+            print('***************user***************', user.username)
+            
+            response = {'message': 'its working'}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'You need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
