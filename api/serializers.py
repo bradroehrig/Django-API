e30ce393a49bdafd4c8d2317724cbef2d5c6d909
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-01-26 17:40:52 -0600

    Commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Movie, Rating
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
