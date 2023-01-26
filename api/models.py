c930e5995fb2c29d796bb70d3dab89b466487fc5
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-01-25 19:33:50 -0600

    commit

--- api/models.py
+++ api/models.py
@@ -6,6 +6,10 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
     
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+    
     def __str__ (self):
         return self.title
 
